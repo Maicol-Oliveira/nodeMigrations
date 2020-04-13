adb2c02d8ad8a70222095b9958c4f28b38ee70e8
maicolc2009_00 <maicol.campos@iv2.com.br>
2020-04-13 16:19:54 -0300

    Add

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+   dialect: 'mysql',
+   host: 'd3drpg.com.br',
+   username: '',
+   password: '',
+   database: '',
+   define: {
+       timestamps: true,
+       underscored: true
+   }
+}
